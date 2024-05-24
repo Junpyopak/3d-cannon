29ad45e6beb4757f1a196436ae1a63912eac10a5
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-05-24 09:22:37 +0900

    커서 마우스 타겟지점

---
+++ Assets/Scripts/ProjectileManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : MonoBehaviour
+{
+    [SerializeField] GameObject objCursor;
+    Camera camMain;
+    void Start()
+    {
+        camMain = Camera.main;
+    }
+
+    
+    void Update()
+    {
+        checkMouse();
+    }
+
+    private void checkMouse()
+    {
+        Ray ray = camMain.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit,100f, LayerMask.GetMask("Ground")) ) 
+        {
+            objCursor.SetActive(true);
+
+            objCursor.transform.position = hit.point + Vector3.up * 0.0001f;
+        }
+        else
+        {
+            objCursor.SetActive(false);
+        }
+    }
+}
